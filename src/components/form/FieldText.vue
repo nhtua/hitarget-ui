7c8df2a75b6dbaf1acba50958371cb082ad829c3
tuanh <hoangtua.vn@gmail.com>
2020-12-28 11:02:58 -0500

    new task form

---
+++ src/components/form/FieldText.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="field">
+    <div class="control">
+      <input :value="modelValue" class="input" type="text" :placeholder="hint">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FieldText',
+  props: {
+    hint: {
+      type: String,
+      require: false,
+      default: ''
+    },
+    modelValue: {
+      type: String,
+      require: true,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,8 +1,27 @@
 <template lang="html">
   <div class="section">
-    <div class="container" name="Summary">
+    <div class="container inner-section" name="Summary">
       <h1 class="title">Today is {{today}}</h1>
-      <h2 class="subtitle">you have {{inProgressCount}} routine task todo</h2>
+      <h2 class="subtitle">
+        you have {{inProgressCount}} routine task{{inProgressCount>1?'s':''}} todo,
+        or you can <span class="tag is-warning" @click="isOpenForm=!isOpenForm">add</span> one more...
+      </h2>
+    </div>
+    <div v-if="isOpenForm" class="container inner-section" name="NewTaskForm">
+      <form @submit.prevent="onSubmit">
+        <FieldText hint="summary"/>
+        <FieldLongText hint="note"/>
+        <div class="field">
+          <p class="control">
+            <button class="button is-primary">
+              <span class="fas fa-plus"></span> <span>Add</span>
+            </button>
+            <button class="button is-light" @click="isOpenForm=false">
+              <span class="fas fa-times"></span> <span>Cancle</span>
+            </button>
+          </p>
+        </div>
+      </form>
     </div>
   </div>
   <div class="section">
@@ -27,12 +46,15 @@
 
 <script>
 import Card from '@/components/Card.vue';
+import FieldText from '@/components/form/FieldText.vue';
+import FieldLongText from '@/components/form/FieldLongText.vue';
 import format from 'date-fns/format';
 export default {
   name: "PageTodo",
-  components: {Card},
+  components: {Card, FieldText, FieldLongText},
   data() {
     return {
+      isOpenForm: false,
       tags: ["docker","kubernetes", "CNCF", "translator"],
       todo: [
         {
@@ -63,24 +85,44 @@ export default {
     inProgressCount() {
       return this.todo.length;
     }
+  },
+  methods: {
+    onSubmit() {
+
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.right-panel {
-  border: solid 1px $soil-dark;
-  color: $soil-light;
-  margin: auto;
-  padding: 10px;
-  &.is-rounded {
-    border-radius: 5px;
+  span.tag {
+    cursor: pointer;
+    &:hover {
+      background: $soil-fire;
+    }
   }
-}
-.tag-list {
-  .hash {
-    color:  $soil-dark;
-    font-style: italic;
+  button {
+    margin-right: 6px;
+    span {
+      margin: 0px 6px;
+    }
   }
-}
+  .inner-section {
+    margin: 15px auto;
+  }
+  .right-panel {
+    border: solid 1px $soil-dark;
+    color: $soil-light;
+    margin: auto;
+    padding: 10px;
+    &.is-rounded {
+      border-radius: 5px;
+    }
+  }
+  .tag-list {
+    .hash {
+      color:  $soil-dark;
+      font-style: italic;
+    }
+  }
 </style>
