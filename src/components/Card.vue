2d6a214690e51d9e3b3c9549d0eaf3be2b072f9d
tuanh <hoangtua.vn@gmail.com>
2020-12-27 22:35:30 -0500

    add button controls stt

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,13 @@
 <template lang="html">
   <div class="box">
     <article class="media">
-      <div class="media-left">
+      <div class="media-left"  @mouseover="showControl=true" @mouseleave="showControl=false">
         <figure class="image is-64x64">
-          <div class="progress-clock is-64x64"><span>80%</span></div>
+          <div  class="progress-clock is-64x64" v-show="!showControl"><span>{{percent}}%</span></div>
+          <div class="progress-control" v-show="showControl" @click.stop="toggleControl">
+            <span v-show="isRunning"><i class="fas fa-running"></i></span>
+            <span v-show="!isRunning"><i class="fas fa-hand-paper"></i></span>
+          </div>
         </figure>
       </div>
       <div class="media-content">
@@ -41,11 +45,24 @@ export default {
       default: null
     }
   },
+  data() {
+    return {
+      isRunning: false,
+      showControl: false,
+      percent: 80,
+    }
+  },
   computed: {
     deadline() {
       if (!this.endDate )
         return null;
-      return format(this.endDate, 'MMM d')
+      return format(this.endDate, 'MMM d');
+    }
+  },
+  methods: {
+    toggleControl() {
+      this.isRunning = !this.isRunning;
+      this.percent += this.percent<100 ?10 :0;
     }
   }
 }
@@ -59,7 +76,8 @@ export default {
       transition: linear 250ms background;
     }
   }
-  .progress-clock  {
+  .progress-clock, .progress-control  {
+    cursor: pointer;
     width: 64px;
     height: 64px;
     line-height: 64px;
