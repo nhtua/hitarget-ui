9f3fb83853283cd73eb5783a6b33879bc26136b4
TuaHN <hoangtua.vn@gmail.com>
2021-02-20 22:28:05 -0500

    small fix default title

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,13 @@ module.exports = {
         additionalData: `@import "@/assets/_vars.scss";`
       }
     }
+  },
+  chainWebpack: config => { //https://stackoverflow.com/a/60402866/1235074
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = 'hiTarget!!'
+      return args
+    })
   }
 }
